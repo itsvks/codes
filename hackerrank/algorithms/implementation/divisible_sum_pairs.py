3bbf09634a7be4ffdef03e5b0f3b7b97cb9b46f6
Vikas Shil <shil.vikas@gmail.com>
2016-09-15 15:58:34 +0530

    dividible sum pairs

---
+++ hackerrank/algorithms/implementation/divisible_sum_pairs.py
@@ -0,0 +1,16 @@
+import sys
+
+def get_number_of_pairs(a, n, k):
+	number_of_pairs = 0
+	for i in range(n):
+		for j in range(i+1, n):
+			if (a[i] + a[j]) % k == 0:
+				print i, j
+				number_of_pairs = number_of_pairs + 1
+	return number_of_pairs
+
+
+n, k = raw_input().strip().split(' ')
+n, k = [int(n), int(k)]
+a = map(int, raw_input().strip().split(' '))
+print get_number_of_pairs(a, n, k)
